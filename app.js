4710a59750b207b155f16b0ce2d542f9e96cc491
liugaxin1118 <liugaxin1118@gmail.com>
2020-08-20 21:09:16 +0800

    init node

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const PORT = process.env.PORT || 3000;
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
@@ -38,6 +39,6 @@ app.get('/search', (req, res) => {
 
 
 // start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
 })
